900fd2338b51bc47f3f9d2115f566ce5d443103a
pragatirahul123 <47949770+pragatirahul123@users.noreply.github.com>
2019-07-08 23:22:32 +0530

    Update more_exercise1.py

--- more_exercise1.py
+++ more_exercise1.py
@@ -1,10 +1,9 @@
-i=1
-while i<=1000:
-    if i%21==0:
-        print"navgurukul",i
-	print "nav",i
-    elif i%7==0:
-        print"gurukul",i
-    elif i%3==0:
-	print "nav",i 
-    i=i+1
+index=1
+while index<=1000:
+    if index%21==0:
+        print"navgurukul",index
+    elif index%7==0:
+        print"gurukul",index
+    elif index%3==0:
+	print "nav",index 
+    index=index+1
